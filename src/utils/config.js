468a2ea8545ca740e03d6ad6350399ac3c9e20e7
Punit Bathija <punit644@gmail.com>
2022-12-06 13:39:08 +0530

    Done working with context, reducers, actions, header, footer

---
+++ src/utils/config.js
@@ -0,0 +1,18 @@
+//firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDoM7VTf2j9IjiNmZuctBqahZes4AdewuQ",
+  authDomain: "contact-book-a6ce0.firebaseapp.com",
+  projectId: "contact-book-a6ce0",
+  storageBucket: "contact-book-a6ce0.appspot.com",
+  messagingSenderId: "918855686537",
+  appId: "1:918855686537:web:e20864a6e25dda0c556958",
+  measurementId: "G-RVLNPT1S56",
+};
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true,
+};
